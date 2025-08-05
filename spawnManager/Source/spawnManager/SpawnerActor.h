09c3ac6b19cb7df611bcacb9f6f597efb5e8b57c
lowey1212 <39388100+lowey1212@users.noreply.github.com>
2025-08-05 16:39:45 +0100

    Revert "Merge pull request #41 from lowey1212/gve4gy-codex/remove-particle-effect-and-add-editor-actor-variable"
    This reverts commit 72763bcf1aca02f8acda28c456d231d1ed608a9e, reversing
    changes made to e006c02e53efa60e25dcba2f1cb035ab3a2fd499.

--- spawnManager/Source/spawnManager/SpawnerActor.h
+++ spawnManager/Source/spawnManager/SpawnerActor.h
@@ -16,7 +16,6 @@ public:
   ASpawnerActor();
 
 #if WITH_EDITORONLY_DATA
-
 protected:
   /** Editor-only preview actor spawned for visualization. Not cooked into builds. */
   UPROPERTY(VisibleAnywhere, Transient, Category = "Visualization",
@@ -25,14 +24,7 @@ protected:
 
   /** Actor class whose instance is spawned for preview */
   UPROPERTY(EditAnywhere, Category = "Visualization")
-
   TSubclassOf<AActor> PreviewActorClass;
-
-protected:
-  /** Editor-only preview actor spawned for visualization. Not cooked into builds. */
-  UPROPERTY(VisibleAnywhere, Transient, Category = "Visualization",
-            meta = (ToolTip = "Editor-only preview actor; not cooked into builds"))
-  TObjectPtr<AActor> PreviewActor;
 #endif
 
 #if WITH_EDITOR
