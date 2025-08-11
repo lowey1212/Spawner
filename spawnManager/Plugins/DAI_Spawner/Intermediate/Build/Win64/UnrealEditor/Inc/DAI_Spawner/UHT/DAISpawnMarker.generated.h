8e6e2fd20a595bdd1dc1e88ea8538783b13dbb0c
lowey1212 <39388100+lowey1212@users.noreply.github.com>
2025-08-11 14:38:57 +0100

    Revert "Merge pull request #69 from lowey1212/codex/fix-include-and-exclude-volumes-functionality"
    This reverts commit 25be9bdfc8b125971a3f7fbf4782aa982188758f, reversing
    changes made to 0ce719cf4969780b00a2cacb63933dae83d70556.

---
+++ spawnManager/Plugins/DAI_Spawner/Intermediate/Build/Win64/UnrealEditor/Inc/DAI_Spawner/UHT/DAISpawnMarker.generated.h
@@ -0,0 +1,54 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+// IWYU pragma: private, include "DAISpawnMarker.h"
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef DAI_SPAWNER_DAISpawnMarker_generated_h
+#error "DAISpawnMarker.generated.h already included, missing '#pragma once' in DAISpawnMarker.h"
+#endif
+#define DAI_SPAWNER_DAISpawnMarker_generated_h
+
+#define FID_PluginTest_spawnManager_Plugins_DAI_Spawner_Source_DAI_Spawner_Public_DAISpawnMarker_h_30_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesADAISpawnMarker(); \
+	friend struct Z_Construct_UClass_ADAISpawnMarker_Statics; \
+public: \
+	DECLARE_CLASS(ADAISpawnMarker, AActor, COMPILED_IN_FLAGS(0 | CLASS_Config), CASTCLASS_None, TEXT("/Script/DAI_Spawner"), NO_API) \
+	DECLARE_SERIALIZER(ADAISpawnMarker)
+
+
+#define FID_PluginTest_spawnManager_Plugins_DAI_Spawner_Source_DAI_Spawner_Public_DAISpawnMarker_h_30_ENHANCED_CONSTRUCTORS \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	ADAISpawnMarker(ADAISpawnMarker&&); \
+	ADAISpawnMarker(const ADAISpawnMarker&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, ADAISpawnMarker); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ADAISpawnMarker); \
+	DEFINE_DEFAULT_CONSTRUCTOR_CALL(ADAISpawnMarker) \
+	NO_API virtual ~ADAISpawnMarker();
+
+
+#define FID_PluginTest_spawnManager_Plugins_DAI_Spawner_Source_DAI_Spawner_Public_DAISpawnMarker_h_27_PROLOG
+#define FID_PluginTest_spawnManager_Plugins_DAI_Spawner_Source_DAI_Spawner_Public_DAISpawnMarker_h_30_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	FID_PluginTest_spawnManager_Plugins_DAI_Spawner_Source_DAI_Spawner_Public_DAISpawnMarker_h_30_INCLASS_NO_PURE_DECLS \
+	FID_PluginTest_spawnManager_Plugins_DAI_Spawner_Source_DAI_Spawner_Public_DAISpawnMarker_h_30_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> DAI_SPAWNER_API UClass* StaticClass<class ADAISpawnMarker>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID FID_PluginTest_spawnManager_Plugins_DAI_Spawner_Source_DAI_Spawner_Public_DAISpawnMarker_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
