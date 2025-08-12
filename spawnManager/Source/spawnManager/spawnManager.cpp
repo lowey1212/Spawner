acddb93dc811a3f313567017b1fce9347ebae95f
lowey1212 <39388100+lowey1212@users.noreply.github.com>
2025-08-12 10:51:28 +0100

    1
    1

--- spawnManager/Source/spawnManager/spawnManager.cpp
+++
@@ -1,5 +0,0 @@
-#include "spawnManager.h"
-#include "Modules/ModuleManager.h"
-
-// Registers the main game module so Unreal can start the game.
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, spawnManager, "spawnManager" );
